3711eb3aca18ecbfb00b03243a1b7423974aad17
Alex Salazar <sjandro16@gmail.com>
2016-09-29 14:36:40 -0700

    Updated tree traversal challenge.

--- trees.py
+++ trees.py
@@ -6,6 +6,15 @@ self.data (the value of the node)
 """
 import sys
 
+class Node(object):
+    """docstring for """
+    def __init__(self, data, left = None ,right = None):
+        super(Node,self).__init__()
+        self.data = data
+        self.left = left
+        self.right = right
+
+
 def inOrder(root):
     if root:
         if root.left:
@@ -30,3 +39,33 @@ def postOrder(root):
             if root.right:
                 postOrder(root.right)
         sys.stdout.write(str(root.data) + " ")
+
+def insertToTree(root, data):
+    if root:
+        if root.data < data:
+            if not root.right:
+                root.right = Node(data)
+            else:
+                insertToTree(root.right, data)
+        else:
+            if not root.left:
+                root.left = Node(data)
+            else:
+                insertToTree(root.left, data)
+
+
+
+def main():
+    head = Node(10)
+
+    for i in [3,2,65,2,12,32,5]:
+        insertToTree(head, i)
+
+    inOrder(head)
+    print "\n"
+    preOrder(head)
+    print "\n"
+    postOrder(head)
+
+if __name__ == '__main__':
+    main()
